bf4caef40662f6be54d7e9c0c9acd97e496375ea
bingcool <2437667702@qq.com>
2026-03-29 11:25:17 +0800

    fixed: Lock组件优化

--- src/Lock/SynchronizeTrait.php
+++ src/Lock/SynchronizeTrait.php
@@ -12,6 +12,7 @@
 namespace Common\Library\Lock;
 
 use Throwable;
+use Swool\Coroutine;
 use malkusch\lock\exception\LockReleaseException;
 
 trait SynchronizeTrait
@@ -23,56 +24,60 @@ trait SynchronizeTrait
      */
     public function synchronized(callable $fn)
     {
+        // accept lock
         $this->lock();
         $codeResult = null;
-        $waitChannel = new \Swoole\Coroutine\Channel(1);
-        $resultChannel = new \Swoole\Coroutine\Channel(1);
+        $waitChannel = new Coroutine\Channel(1);
+        $resultChannel = new Coroutine\Channel(1);
         $returnResultBoolFlag = false;
-        goApp(function () use ($fn, $waitChannel, $resultChannel, &$returnResultBoolFlag) {
-            \Swoole\Coroutine::defer(function () use ($waitChannel, $resultChannel) {
-                try {
-                    $result = $this->releaseLock();
-                } catch (LockReleaseException $lockReleaseException) {
-                }
-                $waitChannel->push(1);
-            });
+        try {
+            goApp(function () use ($fn, $waitChannel, $resultChannel, &$returnResultBoolFlag) {
+                Coroutine::defer(function () use ($waitChannel, $resultChannel) {
+                    try {
+                        $this->releaseLock();
+                    } catch (LockReleaseException $lockReleaseException) {
+                    }
+                    $waitChannel->push(1);
+                });
 
-            try {
-                $codeResult = $fn($this);
-                if (is_bool($codeResult)) {
-                    $codeResult = intval($codeResult);
-                    $returnResultBoolFlag = true;
+                try {
+                    $codeResult = $fn($this);
+                    if (is_bool($codeResult)) {
+                        $codeResult = intval($codeResult);
+                        $returnResultBoolFlag = true;
+                    }
+                    $resultChannel->push($codeResult, 0.1);
+                } catch (\Throwable $exception) {
+                    $resultChannel->push($exception);
                 }
-                $resultChannel->push($codeResult, 0.1);
-            }catch (\Throwable $exception) {
-                $resultChannel->push($exception);
+            });
+        } catch (\Throwable $e) {
+            // goApp() 协程创建失败，在当前协程中释放锁，防止锁泄漏
+            try {
+                $this->releaseLock();
+            } catch (LockReleaseException $lockReleaseException) {
             }
-        });
+            throw $e;
+        }
         /**
-         * $fn执行业务逻辑的时间大于$this->timeOut锁的过期时间时，$waitChannel->pop($this->timeOut) 时间到了，就不会再阻塞了
+         * $fn执行业务逻辑的时间大于$waitChannel->pop($this->timeOut)锁的过期时间时，$waitChannel->pop($this->timeOut) 时间到了，就不会再阻塞了
          * 但$codeResult = $fn($this);业务还在执行, 还没有执行$resultChannel->push($codeResult, 0.1);，所以要判断$resultChannel->length()是否等于0
          * 如果length=0,再循环等待$resultChannel->pop($this->timeOut) 直至获取到结果
         */
         $waitChannel->pop($this->timeOut);
-        $breakFlag = true;
+        $maxRetries = 5;
         $loopTimes = 0;
         do {
-//            if ($loopTimes > 5) {
-//                break;
-//            }
-            if ($resultChannel->length() == 0) {
-                $codeResult = $resultChannel->pop($this->timeOut);
-                if ($codeResult === false) {
-                    $breakFlag = false;
-                } else {
-                    break;
-                }
-                $loopTimes++;
-            } else {
+            if ($resultChannel->length() > 0) {
                 $codeResult = $resultChannel->pop(0.1);
                 break;
             }
-        }while(!$breakFlag);
+            $codeResult = $resultChannel->pop($this->timeOut);
+            if ($codeResult !== false) {
+                break;
+            }
+            $loopTimes++;
+        } while ($loopTimes < $maxRetries);
 
         if ($returnResultBoolFlag === true) {
             if ($codeResult === 1) {

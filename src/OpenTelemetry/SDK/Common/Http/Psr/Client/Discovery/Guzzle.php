86505e8880bdd6ebe90d2d825a66c6e9da692da4
bingcool <2437667702@qq.com>
2026-08-01 13:45:00 +0800

    Update Code

--- src/OpenTelemetry/SDK/Common/Http/Psr/Client/Discovery/Guzzle.php
+++ src/OpenTelemetry/SDK/Common/Http/Psr/Client/Discovery/Guzzle.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Swoolefy\Library\OpenTelemetry\SDK\Common\Http\Psr\Client\Discovery;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use Psr\Http\Client\ClientInterface;
+use Swoolefy\Library\CurlProxy\CurlProxyHandler;
 
 class Guzzle implements DiscoveryInterface
 {
@@ -18,6 +20,12 @@ class Guzzle implements DiscoveryInterface
     #[\Override]
     public function create(mixed $options): ClientInterface
     {
+        if (!isset($options['handler'])) {
+            $stack = HandlerStack::create();
+            CurlProxyHandler::applyPsr7CompatiblePrepareBody($stack);
+            $options['handler'] = $stack;
+        }
+
         return new Client($options);
     }
 }
